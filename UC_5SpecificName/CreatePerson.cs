5e792bb9671450e095a0e4a728ec04379415d0c8
senpooja <poojasen744055@gmail.com>
2022-08-18 12:27:01 +0530

    UC5SpecificName program

---
+++ UC_5SpecificName/CreatePerson.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lamda
+{
+    public class CreatePerson
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        public CreatePerson(string ssn, string name, string address, int age)
+        {
+            SSN = ssn;//Sub System Number
+            SSN = ssn;//Social security number
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
---
+++ UC_5SpecificName/Program.cs
@@ -0,0 +1,17 @@
+﻿using Lamda;
+Console.WriteLine("Welcome to Lamda Expression Problem");
+List<CreatePerson> listPersonInCity = new List<CreatePerson>();
+listPersonInCity.Add(new CreatePerson("456-37-8664", "Vandana", "Sai pragna residency 4 street", 16));
+listPersonInCity.Add(new CreatePerson("488-94-9884", "geetha", "Akgila residency 7 street", 15));
+listPersonInCity.Add(new CreatePerson("933-64-9684", "sneha", "Priya residency 7 street", 18));
+listPersonInCity.Add(new CreatePerson("928-65-4647", "navya", "Geetha residency 7 street", 89));
+listPersonInCity.Add(new CreatePerson("953-74-5474", "gopal", "TTD residency 7 street", 14));
+listPersonInCity.Add(new CreatePerson("235-36-3774", "varshu", "Cv residency 7 street", 65));
+listPersonInCity.Add(new CreatePerson("464-74-7677", "sriya", "Aditya residency 7 street", 38));
+
+Console.WriteLine("\n______________________________________");
+Console.WriteLine("checking personn name present in the list");
+foreach (CreatePerson person in listPersonInCity.FindAll(e => e.Name == "sriya"))
+{
+    Console.WriteLine("Yes,Name exist in your list");
+}
